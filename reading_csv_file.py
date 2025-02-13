a0bcb0a1316a6cd545dde33554b8afc9413ff165
Satish Vishwakarma <staya9125@gmail.com>
2025-02-13 22:01:40 +0530

    Create fake json data

--- reading_csv_file.py
+++ reading_csv_file.py
@@ -3,19 +3,14 @@ import pandas as pd
 
 # Initialize Faker instance
 fake = Faker()
+profiles = []
 
-# Define the number of columns and rows
-num_columns = 20
-num_rows = 10  # You can adjust the number of rows as needed
 
-# Create a list of column names
-columns = [f"Column_{i+1}" for i in range(num_columns)]
+for i in range(10):
+    profile = fake.profile()
+    profiles.append(profile)
 
-# Generate fake data for each row and column
-data = {col: [fake.name() for _ in range(num_rows)] for col in columns}
 
-# Create a DataFrame
-df = pd.DataFrame(data)
+df = pd.DataFrame(profiles)
 
-# Display the DataFrame
-print(df)
+print(df.head(2))
